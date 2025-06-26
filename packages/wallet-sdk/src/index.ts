7a6545da7c4ff88f6059be13daf7e4e9bce5f6fd
Felix Zhang <felix.zhang@coinbase.com>
2025-06-26 14:10:21 -0700

    bring back cSDK

--- packages/wallet-sdk/src/index.ts
+++ packages/wallet-sdk/src/index.ts
@@ -1,5 +1,6 @@
 // Copyright (c) 2018-2024 Coinbase, Inc. <https://www.coinbase.com/>
 export type { AppMetadata, Preference, ProviderInterface } from ':core/provider/interface.js';
 
+export { createCoinbaseWalletSDK } from './createCoinbaseWalletSDK.js';
 export { createProvider } from './interface/builder/core/createProvider.js';
 export { getCryptoKeyAccount, removeCryptoKey } from './kms/crypto-key/index.js';
