abf6c9023e27eb6abed56a471ecad05658109956
Felix Zhang <felix.zhang@coinbase.com>
2025-07-01 11:00:17 -0700

    export

--- packages/account-sdk-ui/src/index.ts
+++
@@ -1,7 +0,0 @@
-export type { SignInWithBaseButtonProps } from './types.js';
-
-// Preact components
-export { SignInWithBaseButton as PreactSignInWithBaseButton } from './components/preact/SignInWithBaseButton.js';
-
-// React components
-export { SignInWithBaseButton as ReactSignInWithBaseButton } from './components/react/SignInWithBaseButton.js';
