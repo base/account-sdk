2034ec69a0cf5dadf8a744583f8aea2095ee2e63
Felix Zhang <felix.zhang@coinbase.com>
2025-07-07 15:43:49 -0700

    bring back addOwner

--- packages/account-sdk/src/interface/builder/core/createBaseAccountSDK.ts
+++ packages/account-sdk/src/interface/builder/core/createBaseAccountSDK.ts
@@ -6,9 +6,12 @@ import {
   SubAccountOptions,
 } from ':core/provider/interface.js';
 import { loadTelemetryScript } from ':core/telemetry/initCCA.js';
+import { abi } from ':sign/base-account/utils/constants.js';
 import { store } from ':store/store.js';
+import { assertPresence } from ':util/assertPresence.js';
 import { checkCrossOriginOpenerPolicy } from ':util/checkCrossOriginOpenerPolicy.js';
 import { validatePreferences, validateSubAccount } from ':util/validatePreferences.js';
+import { decodeAbiParameters, encodeFunctionData, toHex } from 'viem';
 import { BaseAccountProvider } from './BaseAccountProvider.js';
 
 export type CreateProviderOptions = Partial<AppMetadata> & {
@@ -73,7 +76,7 @@ export function createBaseAccountSDK(params: CreateProviderOptions) {
 
   let provider: ProviderInterface | null = null;
 
-  return {
+  const sdk = {
     getProvider: () => {
       if (!provider) {
         provider = new BaseAccountProvider(options);
@@ -81,5 +84,61 @@ export function createBaseAccountSDK(params: CreateProviderOptions) {
 
       return provider;
     },
+    subAccount: {
+      addOwner: async ({
+        address,
+        publicKey,
+        chainId,
+      }: {
+        address?: `0x${string}`;
+        publicKey?: `0x${string}`;
+        chainId: number;
+      }) => {
+        const subAccount = store.subAccounts.get();
+        const account = store.account.get();
+        assertPresence(account, new Error('account does not exist'));
+        assertPresence(subAccount?.address, new Error('subaccount does not exist'));
+
+        const calls = [];
+        if (publicKey) {
+          const [x, y] = decodeAbiParameters([{ type: 'bytes32' }, { type: 'bytes32' }], publicKey);
+          calls.push({
+            to: subAccount.address,
+            data: encodeFunctionData({
+              abi,
+              functionName: 'addOwnerPublicKey',
+              args: [x, y] as const,
+            }),
+            value: toHex(0),
+          });
+        }
+
+        if (address) {
+          calls.push({
+            to: subAccount.address,
+            data: encodeFunctionData({
+              abi,
+              functionName: 'addOwnerAddress',
+              args: [address] as const,
+            }),
+            value: toHex(0),
+          });
+        }
+
+        return (await sdk.getProvider()?.request({
+          method: 'wallet_sendCalls',
+          params: [
+            {
+              calls,
+              chainId: toHex(chainId),
+              from: account.accounts?.[0],
+              version: '1',
+            },
+          ],
+        })) as string;
+      },
+    },
   };
+
+  return sdk;
 }
