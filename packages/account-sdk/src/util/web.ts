3069009373392771fef3456ad21732cb9b3be2df
Felix Zhang <felix.zhang@coinbase.com>
2025-07-09 11:51:02 -0700

    oops

--- packages/account-sdk/src/util/web.ts
+++ packages/account-sdk/src/util/web.ts
@@ -34,7 +34,7 @@ export function openPopup(url: URL): Promise<Window> {
     return popup;
   }
 
-  const popup = null;
+  const popup = tryOpenPopup();
 
   // If the popup was blocked, show a snackbar with a retry button
   if (!popup) {
