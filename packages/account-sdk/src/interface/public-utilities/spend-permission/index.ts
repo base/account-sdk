44488f2518b980c24b694ff8e6e8edc87d15d0f8
Spencer Stock <spencer.stock@coinbase.com>
2025-08-21 00:09:11 -0600

    Add fetch permission RPC method and public utility

--- packages/account-sdk/src/interface/public-utilities/spend-permission/index.ts
+++ packages/account-sdk/src/interface/public-utilities/spend-permission/index.ts
@@ -1,3 +1,4 @@
+export * from './methods/fetchPermission.js';
 export * from './methods/fetchPermissions.js';
 export * from './methods/getHash.js';
 export * from './methods/getPermissionStatus.js';
@@ -5,3 +6,4 @@ export * from './methods/prepareRevokeCallData.js';
 export * from './methods/prepareSpendCallData.js';
 export * from './methods/requestRevoke.js';
 export * from './methods/requestSpendPermission.js';
+
