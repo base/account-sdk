b00fde0fe2599a779dbc862ab9202dfa5a5f9ef2
Spencer Stock <spencer.stock@coinbase.com>
2025-09-08 12:38:27 -0600

    format

--- packages/account-sdk/src/interface/public-utilities/spend-permission/methods/prepareSpendCallData.test.ts
+++ packages/account-sdk/src/interface/public-utilities/spend-permission/methods/prepareSpendCallData.test.ts
@@ -1,7 +1,7 @@
 import { SpendPermission } from ':core/rpc/coinbase_fetchSpendPermissions.js';
 import {
-    spendPermissionManagerAbi,
-    spendPermissionManagerAddress,
+  spendPermissionManagerAbi,
+  spendPermissionManagerAddress,
 } from ':sign/base-account/utils/constants.js';
 import { Address, Hex, encodeFunctionData } from 'viem';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
