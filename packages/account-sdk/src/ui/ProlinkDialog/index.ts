3f83a87aeee78c23b276226fa1892eb18f6d7045
Spencer Stock <spencerstock@gmail.com>
2025-10-21 16:57:11 -0600

    remove dialog

--- packages/account-sdk/src/ui/ProlinkDialog/index.ts
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2018-2025 Coinbase, Inc. <https://www.coinbase.com/>
-
-export { ProlinkDialog } from './ProlinkDialog.js';
-export type { ProlinkDialogProps } from './ProlinkDialog.js';
