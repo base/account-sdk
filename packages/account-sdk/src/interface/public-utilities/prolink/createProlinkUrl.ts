9f697930e11877ffbeeb0e179076a576c1d3f52a
Brendan Forster <brendan.forster@coinbase.com>
2025-11-04 00:19:58 +0900

    update docstring

--- packages/account-sdk/src/interface/public-utilities/prolink/createProlinkUrl.ts
+++ packages/account-sdk/src/interface/public-utilities/prolink/createProlinkUrl.ts
@@ -10,19 +10,19 @@
  * @param prolink - Base64url-encoded prolink payload
  * @param url - URL to use for the link, defaults to https://base.app/base-pay
  * @param additionalQueryParams - Additional query parameters to add to the link
- * @returns { link: string } - Object containing the full link
+ * @returns string - The full link
  *
  * @example
  * ```typescript
  * const prolink = await encodeProlink(request);
  * const link = createProlinkUrl(prolink);
- * // Returns: { link: 'https://base.app/base-pay?p=CAEQ...' }
+ * // Returns: 'https://base.app/base-pay?p=CAEQ...'
  *
  * const linkWithAdditionalParams = createProlinkUrl(prolink, 'https://base.app/base-pay', {
  *   foo: 'bar',
  *   baz: 'qux',
  * });
- * // Returns: { link: 'https://base.app/base-pay?p=CAEQ...&foo=bar&baz=qux' }
+ * // Returns: 'https://base.app/base-pay?p=CAEQ...&foo=bar&baz=qux'
  * ```
  */
 export function createProlinkUrl(
