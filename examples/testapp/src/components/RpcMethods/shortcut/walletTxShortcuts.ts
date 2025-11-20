9ab5a4eae1ae2d00b3d92d5061c49d68809d8696
cb-jake <95890768+cb-jake@users.noreply.github.com>
2025-11-20 13:20:50 -0700

    add datacallback shortcut (#184)
    * add datacallback shortcut
    
    * format

--- examples/testapp/src/components/RpcMethods/shortcut/walletTxShortcuts.ts
+++ examples/testapp/src/components/RpcMethods/shortcut/walletTxShortcuts.ts
@@ -16,6 +16,39 @@ const walletSendCallsShortcuts: ShortcutType[] = [
       },
     },
   },
+  {
+    key: 'data_callback',
+    data: {
+      chainId: CHAIN_ID_TO_FILL,
+      from: ADDR_TO_FILL,
+      calls: [],
+      version: '1',
+      capabilities: {
+        paymaster: {
+          url: 'https://paymaster.base.org',
+        },
+        dataCallback: {
+          requests: [
+            {
+              type: 'email' as const,
+              optional: false,
+            },
+            {
+              type: 'physicalAddress' as const,
+              optional: true,
+            },
+            {
+              type: 'phoneNumber' as const,
+              optional: false,
+            },
+            {
+              type: 'name' as const,
+            },
+          ],
+        },
+      },
+    },
+  },
 ];
 
 export const walletTxShortcutsMap = {
